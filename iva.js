34bf4eb184ff5dc00f481292bdc81e212a9d1e3a
Miguel Díaz <mdiazl@vectoritcgroup.com>
2021-05-25 13:32:29 +0200

    Agrego funciones

--- iva.js
+++ iva.js
@@ -11,8 +11,22 @@ let precio = 50;
 
 let importe;
 
-importe = precio + (precio * ivaNormal/100);
+function calcularCuota (base, iva = 10) {
+    return base * iva /100;
+}
 
-console.log("El precio del producto sin IVA es: " + precio);
-console.log("El precio del producto con IVA es: " + importe);
+function calcularPrecio (base, cuota) {
 
+    return base + cuota;
+}
+
+// importe = precio + (precio * ivaNormal/100);
+
+// console.log("El precio del producto sin IVA es: " + precio);
+// console.log("El precio del producto con IVA es: " + importe);
+
+let cuota = calcularCuota(precio, ivaNormal);
+calcularPrecio(precio, cuota);
+
+console.log (calcularCuota(precio, ivaNormal));
+console.log (calcularPrecio(precio, cuota));
